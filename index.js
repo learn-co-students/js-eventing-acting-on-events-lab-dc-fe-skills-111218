12da0dde1142ad54680fda8d1bf00bfa8e02a28c
Greyson Harris <gharris@esri.com>
2019-01-09 03:22:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#ff5bab";
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 5}px`
+  }
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+ 
+  if (left < 360) {
+    dodger.style.left = `${left + 5}px`
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft();
+  }
+})
+
+document.addEventListener("keydown", function(e) {
+  if (e.which === 39) {
+    moveDodgerRight();
+  }
+})
